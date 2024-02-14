05b6858a483800919dbfe275b1210e6dda97c56b
Giedrius77 <giedrius.zalionis@gmail.com>
2024-02-14 12:54:15 +0200

    task views, detail views, mark done uzduotys

--- tasker_giedrius/tasks/views.py
+++ tasker_giedrius/tasks/views.py
@@ -1,8 +1,9 @@
 # kuriame nauja front end view vietoj raketos
 # kuris yra vienas puslapis
 
+from django.contrib import messages
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from . import models
 
 # web browser agrindinio puslapio views
@@ -17,6 +18,20 @@ def index(request: HttpRequest) -> HttpResponse:
 
 def task_list(request: HttpRequest) -> HttpResponse:
     return render(request, 'tasks/task_list.html', {
-        'task_list': models.Task.objects.all()
+        'task_list': models.Task.objects.all(),
     })
- 
+
+def task_detail(request: HttpRequest, pk:int) -> HttpResponse:
+    return render(request, 'tasks/task_detail.html', {
+        'task': get_object_or_404(models.Task, pk=pk)
+    })
+
+def task_done(request: HttpRequest, pk: int) -> HttpResponse:
+    task = get_object_or_404(models.Task, pk=pk)
+    task.is_done = not task.is_done
+    task.save()
+    messages.success(request, f"Task #{task.name} marked as {'done' if task.is_done else 'undone'}.") #galima perduoti task.name vietoj {task.pk}
+    if request.GET.get('next'):
+        return redirect(request.GET.get('next'))
+    return redirect(task_list)
+  
