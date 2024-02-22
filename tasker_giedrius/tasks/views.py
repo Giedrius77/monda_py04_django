742257d39942afd828475ce33f6b350bcfbfb866
Giedrius77 <giedrius.zalionis@gmail.com>
2024-02-22 12:06:12 +0200

    project_created.html sukurtas

--- tasker_giedrius/tasks/views.py
+++ tasker_giedrius/tasks/views.py
@@ -40,7 +40,7 @@ def index(request: HttpRequest) -> HttpResponse:
     context = {
         'projects_count': models.Project.objects.count(),
         'tasks_count': models.Task.objects.count(),
-        'users_count': models.get_user_model().objects.count(),
+        'users_count': models.get_user_model().objects.count(), 
     }
     return render(request, 'tasks/index.html', context)
 
